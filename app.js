7eb7dabbdc8b442a48087a8e126945e7c7a68c53
yokoi.shinya <shinya.yokoi@gmail.com>
2015-01-26 12:18:38 +0900

    test2

--- app.js
+++ app.js
@@ -1,33 +1,62 @@
 Ext.application({
-    launch: function() {
-        Ext.Viewport.add({
-            xtype: 'navigationview',
-            items: [{
-                xtype: 'panel',
-                title: 'First View',    // ナビゲーションバーに表示する文字列
-                items : [
-                {
+  launch: function() {
+    Ext.Viewport.add({
+      xtype: 'navigationview',
+        items: [
+          {//panel_1
+            xtype: 'panel',
+            title: 'Program mode',    // ナビゲーションバーに表示する文字列
+            items : [//start item
+            {
+              xtype: 'label',
+              html: 'Program mode'
+            },
+            {
+              xtype: 'button',
+              text: 'Push Next View',
+                  
+              // ボタンにイベントを設定
+              handler: function() {
+
+                // このコンポーネントの2つ上の navigationview で定義されたメソッド
+                this.parent.parent.push({
+                  xtype: 'panel',
+                  title: 'Second View',
+                  items: [{
                     xtype: 'label',
-                    html: 'This is First View'
-                },{
-                    xtype: 'button',
-                    text: 'Push Next View',
-                   
+                    html: 'This is Second View' // 画面遷移後にナビゲーションバーに表示する文字列
+                   }]
+                });
+               }
+             },
+            {
+              xtype: 'toolbar',
+            　items: [{
+            　   xtype: 'button',
+                 iconCls: 'add',
+                 iconMask: true,
+                 badgeText: '2',
+                 ui: 'action',
+      
+                 // ボタンにイベントを設定
+                 handler: function() {
+
+                  this.parent.add({
+               　   xtype: 'button',
+                    iconCls: 'add',
+                    iconMask: true,
+                    ui: 'action',
                     // ボタンにイベントを設定
                     handler: function() {
-
-                        // このコンポーネントの2つ上の navigationview で定義されたメソッド
-                        this.parent.parent.push({
-                            xtype: 'panel',
-                            title: 'Second View',
-                            items: [{
-                                xtype: 'label',
-                                html: 'This is Second View' // 画面遷移後にナビゲーションバーに表示する文字列
-                            }]
-                        });
-                    }
-                }]
-            }]
-        });
-    }
+                        this.parent.push({
+                    });}
+                  });
+                 }
+              }]
+            }
+          ]//end item
+          }]//navigationview item
+    });//Ext.Viewport
+  }
 });
+
